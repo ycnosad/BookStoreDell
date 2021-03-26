2a1b133855231781ef0890c8b4171858bf76b31e
yuanchang666 <18779105287@163.com>
2021-03-26 18:47:14 +0800

    解决冲突第二次

--- src/Acme.BookStore.Application/BookMarks/BookMarkAppService.cs
+++ src/Acme.BookStore.Application/BookMarks/BookMarkAppService.cs
@@ -38,7 +38,7 @@ namespace Acme.BookStore.BookMarks
             var books = await _bookRepository.GetListAsync();
             return new ListResultDto<BookLookupDto> (ObjectMapper.Map<List<Book>, List<BookLookupDto>>(books));
         }
-
+        //高点冲突啊
         public override async Task<PagedResultDto<BookMarkDTO>> GetListAsync(PagedAndSortedResultRequestDto input)
         {
             //Set a default sorting, if not provided
